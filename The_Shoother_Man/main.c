aa05e56a813862b9b9ef0606afe9e6a3694df3db
GABRIEL AUGUSTO FABRI SOLTOVSKI <gafs22@inf.ufpr.br>
2025-06-04 17:54:42 -0300

    Indo embora, continuo em casa a situação

--- The_Shoother_Man/main.c
+++ The_Shoother_Man/main.c
@@ -1,1 +1,16 @@
 #include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+
+#define X_SCREEN 800
+#define Y_SCREEN 600
+
+int main ()
+{
+    al_init();
+    al_install_keyboard();
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0/30.0);
+
+    ALLEGRO_EVENT_QUEUE* queue();
+}
